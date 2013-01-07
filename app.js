d6311efa1aeb77d523937f0fc96e7de38e4509b2
William Youmans <will@williamyoumans.com>
2013-01-06 19:21:11 -0700

    added node server and main view

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var port = 8080;
+
+app.configure(function(){
+  app.set('view engine', 'jade');
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function(req, res){
+  res.render("index.jade", {layout:false});
+});
+
+app.listen(port);
+
+console.log('Node server listening on port ' + port);
